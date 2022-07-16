28daed310c481d3e9d502963d0b19357ecef8117
aayush-aryan <log2aayusharyan@gmail.com>
2022-07-16 17:31:08 +0530

    added. Welcome message

---
+++ LineComprasionProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComprasionProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line Computation Program :");
+        }
+    }
+}
