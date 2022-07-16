b2ace0d43c62b7841bac8d86cf392c9431bd03c6
aayush-aryan <log2aayusharyan@gmail.com>
2022-07-16 20:31:16 +0530

    added. comparing Two lines are equal or not

--- LineComprasionProblem/Program.cs
+++ LineComprasionProblem/Program.cs
@@ -10,7 +10,7 @@ namespace LineComprasionProblem
             Console.WriteLine("Enter the X1 cooridinate");
             int x1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine( "Enter the Y1-cordinate:");
+            Console.WriteLine("Enter the Y1-cordinate:");
             int y1 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Enter the x2-cordinate:");
@@ -19,10 +19,20 @@ namespace LineComprasionProblem
             Console.WriteLine("Enter the Y2-cordinate:");
             int y2 = Convert.ToInt32(Console.ReadLine());
 
-            double lengthOfLine = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2));
+            double lengthOfLine1 = Math.Sqrt(Math.Pow((x1 - 0), 2) + Math.Pow((y1 - 0), 2));
 
-            Console.WriteLine("Lenth of the line : " + lengthOfLine + "cm");
-              
+            double lengthOfLine2 = Math.Sqrt(Math.Pow((x2 - 0), 2) + Math.Pow((y2 - 0), 2));
+
+
+            //Console.WriteLine(lengthOfLine1.Equals(lengthOfLine2));
+            if (lengthOfLine1.Equals(lengthOfLine2))
+            {
+                Console.WriteLine("Both lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Both lines are not equls");
+            }
 
 
 
