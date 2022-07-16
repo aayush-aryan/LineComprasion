174ec29831caa9964bfcc6e51dcc28484f5d0e5c
aayush-aryan <log2aayusharyan@gmail.com>
2022-07-16 19:49:30 +0530

    added. calculating lenth of line

--- LineComprasionProblem/Program.cs
+++ LineComprasionProblem/Program.cs
@@ -7,6 +7,27 @@ namespace LineComprasionProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to line Computation Program :");
+            Console.WriteLine("Enter the X1 cooridinate");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine( "Enter the Y1-cordinate:");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the x2-cordinate:");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Y2-cordinate:");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            double lengthOfLine = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2));
+
+            Console.WriteLine("Lenth of the line : " + lengthOfLine + "cm");
+              
+
+
+
+
+
         }
     }
 }
