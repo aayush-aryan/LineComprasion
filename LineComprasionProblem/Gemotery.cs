ea4dfbaef10214a42343c7c33a10b9cc54fdc274
aayush-aryan <log2aayusharyan@gmail.com>
2022-07-23 00:35:32 +0530

    added. using oops concept

---
+++ LineComprasionProblem/Gemotery.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComprasionProblem
+{
+    class Gemotery
+    {
+        public static void LineComprasion(int x1,int y1,int x2,int y2)
+        {
+            double lengthOfLine1 = Math.Sqrt(Math.Pow((x1 - 0), 2) + Math.Pow((y1 - 0), 2));
+
+            double lengthOfLine2 = Math.Sqrt(Math.Pow((x2 - 0), 2) + Math.Pow((y2 - 0), 2));
+
+            int relativeValueOfInstance = lengthOfLine1.CompareTo(lengthOfLine2);
+
+            if (relativeValueOfInstance == -1)
+            {
+                Console.WriteLine("Line1 is smaller then Line2 ");
+            }
+            else if (relativeValueOfInstance == 1)
+            {
+                Console.WriteLine("Line1 is greater then Line2 ");
+            }
+            else
+            {
+                Console.WriteLine("both lines are equal");
+            }
+        }
+    }
+}
--- LineComprasionProblem/Program.cs
+++ LineComprasionProblem/Program.cs
@@ -19,24 +19,8 @@ namespace LineComprasionProblem
             Console.WriteLine("Enter the Y2-cordinate:");
             int y2 = Convert.ToInt32(Console.ReadLine());
 
-            double lengthOfLine1 = Math.Sqrt(Math.Pow((x1 - 0), 2) + Math.Pow((y1 - 0), 2));
+            Gemotery.LineComprasion(x1, y1, x2, y2);
 
-            double lengthOfLine2 = Math.Sqrt(Math.Pow((x2 - 0), 2) + Math.Pow((y2 - 0), 2));
-
-            int relativeValueOfInstance = lengthOfLine1.CompareTo(lengthOfLine2);
-            
-            if(relativeValueOfInstance == -1)
-            {
-                Console.WriteLine("Line1 is smaller then Line2 ");
-            }else if(relativeValueOfInstance == 1)
-            {
-                Console.WriteLine("Line1 is greater then Line2 ");
-            }
-            else
-            {
-                Console.WriteLine("both lines are equal");
-            }
-           
         }
     }
 }
